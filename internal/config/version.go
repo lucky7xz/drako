792e575871e49426d14f4317c05650357b9a662a
lucky7xz <ml.pojoni42@outlook.com>
2026-01-30 00:27:53 +0100

    updated descriptions, log history command

--- internal/config/version.go
+++ internal/config/version.go
@@ -6,4 +6,4 @@ const AppName = "lucky7xz/drako"
 // Version is the current version of the application.
 // This variable can be overwritten at build time using -ldflags.
 // Example: go build -ldflags "-X 'github.com/lucky7xz/drako/internal/config.Version=v1.0.0'"
-var Version = "v0.2.8-alpha"
+var Version = "v0.2.9-alpha"
